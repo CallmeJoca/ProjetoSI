69e0697503c43b1a69fc2a85be8ff69f34fc9b2a
Henrique Albuquerque <45293489+Henrique-Albuquerque@users.noreply.github.com>
2019-12-18 15:31:45 +0000

    agr ja tem cliente

---
+++ Server/src/server/Cliente.java
@@ -0,0 +1,22 @@
+package server;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+
+public class Cliente {
+
+    public static void main(String[] args) {
+        
+        try {
+            Socket s = new Socket("192.168.43.27", 6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("Hello Server");
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
