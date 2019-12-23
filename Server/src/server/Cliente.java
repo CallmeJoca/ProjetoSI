65cc3086bed64f6edd065d13fa8c0a6d96221502
tyagomarques <43614367+tyagomarques@users.noreply.github.com>
2019-12-23 22:16:59 +0000

    Começando a trabalhar...

--- Server/src/server/Cliente.java
+++ Server/src/server/Cliente.java
@@ -9,7 +9,7 @@ public class Cliente {
     public static void main(String[] args) {
         
         try {
-            Socket s = new Socket("192.168.43.27", 6666);
+            Socket s = new Socket("192.168.1.75", 6666);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             dout.writeUTF("Hello Server");
             dout.flush();
@@ -20,3 +20,7 @@ public class Cliente {
         }
     }
 }
+
+//192.168.43.27 doutra pessoa
+//192.168.1.75 computer
+//192.168.1.84 laptop
