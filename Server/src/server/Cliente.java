7c102e3c7cffda66bcf74af6962b623d54aa4247
tyagomarques <tyagomarques1999@gmail.com>
2019-12-23 22:34:05 +0000

    Melhorando o cliente..

--- Server/src/server/Cliente.java
+++ Server/src/server/Cliente.java
@@ -2,22 +2,27 @@ package Server.src.server;
 
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Cliente {
 
     public static void main(String[] args) {
-        
-        try {
-            Socket s = new Socket("192.168.1.75", 6666);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            dout.writeUTF("Hello Server");
-            dout.flush();
-            dout.close();
-            s.close();
-        } catch(Exception e) {
-            System.out.println(e);
-        }
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        do {
+            try {
+                input = scanner.next();
+                Socket s = new Socket("192.168.1.84", 6666);
+                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+                dout.writeUTF(input);
+                dout.flush();
+                dout.close();
+                s.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        } while (!input.equals("end")) ;
     }
 }
 
