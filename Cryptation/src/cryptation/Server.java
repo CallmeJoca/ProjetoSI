e50daec9d4720a93a851e52638d34978083077f5
Margarida <margaridaq99@gmail.com>
2020-01-02 21:49:29 +0000

    server e client

---
+++ Cryptation/src/cryptation/Server.java
@@ -0,0 +1,38 @@
+package cryptation;
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    
+    public static void main(String[] args) {
+        String str = null;
+        ServerSocket ss = null;
+        try {
+              ss = new ServerSocket(6666);
+            do{
+                try{
+                    Socket s = ss.accept();
+                    DataInputStream dis = new DataInputStream(s.getInputStream());
+                    str = (String) dis.readUTF();
+                    System.out.println("Client Says = " + str);
+                }
+                catch(EOFException exc) {
+                    continue;
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                    break;
+                }
+            
+
+            }while(!str.equals("end"));
+            ss.close();
+        }
+        catch ( Exception e ) { System.out.println(e); }
+    }
+    
+}
+
