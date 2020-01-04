e21415b5e070f0a08c5fcb62bff77633fd766364
CallmeJoca <jocapissarra@protonmail.com>
2020-01-04 19:47:22 +0000

    Update Server.java

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -8,38 +8,15 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Server implements Runnable{
+public class Server{
+    
+    Socket clientInput;
 
-    static void main(String[] args) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    ServerSocket sSocket;
-    Thread t;
-    ArrayList[] clients;
-    
-    public Server(int port) throws IOException{
-        
-        sSocket = new ServerSocket(port);
-        t = new Thread(this);
-        t.start();
-    }
-    public int printMenu(Socket clientInput) throws IOException, ClassNotFoundException{
-        ObjectInputStream inputText = new ObjectInputStream(clientInput.getInputStream());
-        
-        System.out.println("Menu:");
-        System.out.println("1. Escolher porta a abrir");
-        System.out.println("2. Fazer print dos clientes");
-        System.out.println("3. Desligar");
-        
-        return ((int) inputText.readObject());
-    }
-    @Override
-    public void run() {
+    static void main() {
         while(true) {
             try {
                 int option;
-                Socket clientInput = sSocket.accept();
+                clientInput = sSocket.accept();
                 option = printMenu(clientInput);
                 
                 switch (option) {
@@ -61,5 +38,25 @@ public class Server implements Runnable{
             }
         }
     }
-    
+
+    ServerSocket sSocket;
+    ArrayList[] clients;
+
+    /**
+     *
+     * @param clientInput
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static int printMenu(Socket clientInput) throws IOException, ClassNotFoundException{
+        ObjectInputStream inputText = new ObjectInputStream(clientInput.getInputStream());
+        
+        System.out.println("Menu:");
+        System.out.println("1. Escolher porta a abrir");
+        System.out.println("2. Fazer print dos clientes");
+        System.out.println("3. Desligar");
+        
+        return ((int) inputText.readObject());
+    }
 }
