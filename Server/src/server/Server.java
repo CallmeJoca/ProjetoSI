101c38d9582a626adf0149db9e9f186df749e2b1
CallmeJoca <jocapissarra@protonmail.com>
2020-01-04 18:22:31 +0000

    small server update

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -2,27 +2,65 @@ package server;
 
 import java.io.DataInputStream;
 import java.io.EOFException;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
-public class Server {
+public class Server implements Runnable{
+    
+    ServerSocket sSocket;
+    Thread t;
+    ArrayList[] clients;
+    
+    public Server(int port) throws IOException{
+        
+        sSocket = new ServerSocket(port);
+        t = new Thread(this);
+        t.start();
+    }
     
     
-    public int printMenu(){
-        int option = -1;
+    public int printMenu(Socket clientInput) throws IOException, ClassNotFoundException{
+        ObjectInputStream inputText = new ObjectInputStream(clientInput.getInputStream());
         
         System.out.println("Menu:");
         System.out.println("1. Escolher porta a abrir");
         System.out.println("2. Fazer print dos clientes");
         System.out.println("3. Desligar");
         
-        
-        return option;
+        return ((int) inputText.readObject());
     }
     
-    public static void main(String[] args) {
-        
-        
+    /**
+     *
+     * @param args
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public void run(String[] args) throws IOException, ClassNotFoundException {
+          
+        while(true) {
+            int option = 0;
+            Socket clientInput = sSocket.accept();
+            option = printMenu(clientInput);
+            
+            if (option == 1){
+                
+            }
+            else if (option == 2) {
+                
+            }
+            else if (option == 3) {
+                
+            }
+            else{
+                
+            }
+        }
+          
 //        String str = null;
 //        ServerSocket ss = null;
 //        try {
@@ -47,6 +85,11 @@ public class Server {
 //            ss.close();
 //        }
 //        catch ( Exception e ) { System.out.println(e); }
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
