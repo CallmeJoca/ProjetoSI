bc93af0e0bb1d6203fab42d168fa40557b814477
Margarida <margaridaq99@gmail.com>
2020-01-05 14:39:40 +0000

    PBKDF2 encrypt

--- Server/src/server/PBKDF2.java
+++ Server/src/server/PBKDF2.java
@@ -1,44 +1,63 @@
 package server;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Random;
-import javax.crypto.SecretKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 
+import java.util.Random;
+import javax.crypto.BadPaddingException;
+
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class PBKDF2 {
     
     private static final Random RANDOM = new SecureRandom();
     
-    public PBKDF2() {
-        
-    }
+    public PBKDF2() { }
     
     // Geração de 'salt'
-    public byte[] getNextSalt() {
-        byte[] salt = new byte[64];
+    public static byte[] getNextSalt() {
+        byte[] salt = new byte[16];
         RANDOM.nextBytes(salt);
         return salt;
     }
     
     // Calcula e devolve PBKDF2 da password em parâmetro.
-    public String getPBKDF2(String password) throws Exception {
+    public static String getPBKDF2(String password) throws Exception {
         
         char[] chars = password.toCharArray();
-        byte[] salt = this.getNextSalt();
+        byte[] salt = PBKDF2.getNextSalt();
         int iterations = 10000;
         
-        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 256);
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 256); // AES-256
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
         byte[] key = skf.generateSecret(spec).getEncoded();
         
-        return key.toString();
+        return new String(key);
     }
     
-    public String cifrarComPBKDF2(String mensagem, SecretKey chave) {
-        // TODO: PBKDF2 com um atributo chave que é inicializado no construtor
-        // ou passar como parâmetro para aqui cifrar
-        return "Couves";
+    // O segundo parâmetro (PBKDF2_from_user_pw) deve ser obtido através do retorno 
+    // da função getPBKDF2 com parâmetro 'PBKDF2_from_user_pw' = password 
+    // escolhida pelo utilizador.
+    public static byte[] cifrarComPBKDF2(String mensagem, String PBKDF2_from_user_pw) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        
+        Cipher cipher = Cipher.getInstance("AES");
+
+        KeySpec spec = new PBEKeySpec(PBKDF2_from_user_pw.toCharArray(), getNextSalt(), 65536, 256); // AES-256
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] key = f.generateSecret(spec).getEncoded();
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encValue = cipher.doFinal(mensagem.getBytes());
+        
+        return encValue;
     }
 }
