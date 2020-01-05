c3e3162810f1559f700911df08896128bd2eb8c0
Margarida <margaridaq99@gmail.com>
2020-01-05 17:03:21 +0000

    Update PBKDF2.java

--- Server/src/server/PBKDF2.java
+++ Server/src/server/PBKDF2.java
@@ -1,10 +1,13 @@
 package server;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.KeySpec;
+import java.util.Base64;
 
 import java.util.Random;
 import javax.crypto.BadPaddingException;
@@ -13,12 +16,14 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.ShortBufferException;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class PBKDF2 {
     
     private static final Random RANDOM = new SecureRandom();
+    private static final String HEXES = "0123456789ABCDEF";
     
     public PBKDF2() { }
     
@@ -44,9 +49,8 @@ public class PBKDF2 {
     }
     
     // O segundo parâmetro (PBKDF2_from_user_pw) deve ser obtido através do retorno 
-    // da função getPBKDF2 com parâmetro 'PBKDF2_from_user_pw' = password 
-    // escolhida pelo utilizador.
-    public static byte[] cifrarComPBKDF2(String mensagem, String PBKDF2_from_user_pw) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    // da função getPBKDF2 com parâmetro 'password' = password escolhida pelo utilizador.
+    public static String cifrarComPBKDF2(String mensagem, String PBKDF2_from_user_pw) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, InvalidParameterSpecException, ShortBufferException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException {
         
         Cipher cipher = Cipher.getInstance("AES");
 
@@ -54,10 +58,14 @@ public class PBKDF2 {
         SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
         byte[] key = f.generateSecret(spec).getEncoded();
         SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
-        
+
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         byte[] encValue = cipher.doFinal(mensagem.getBytes());
         
-        return encValue;
+        // Encode using BASE64
+        String encodeinput = Base64.getEncoder().encodeToString(encValue);
+        
+        return encodeinput;
     }
+    
 }
